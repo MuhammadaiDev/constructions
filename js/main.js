9e977baabc282f389c03d796f06aa1e532cd1e04
Muhammad Arshad <arshadaidev@gmail.com>
2020-06-04 23:02:57 -0700

    Done

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function checkWidth() {
-    if ($(window).width() < 514) {
-        $('#menu2').addClass('f-nav');
-    } else {
-        $('#menu2').removeClass('f-nav');
-    }
-}
-
-$(window).resize(checkWidth);
